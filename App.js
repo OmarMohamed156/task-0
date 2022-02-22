f8e6a2b91d29c14d1448435802df9a991b9947b3
OmarMohamed156 <om658036@gmail.com>
2022-02-22 13:16:26 +0200

    added victory native charting library

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React ,{useEffect, useState} from "react";
 import {
   Text,
   Link,
@@ -11,19 +11,45 @@ import {
   extendTheme,
   VStack,
   Code,
+  Button,
+  Toast
 } from "native-base";
 import NativeBaseIcon from "./components/NativeBaseIcon";
+import { VictoryBar, VictoryChart, VictoryLegend, VictoryTheme, Victoryl, VictoryScatter, VictoryLine, VictoryZoomContainer
+} from "victory-native";
 
 // Define the config
 const config = {
   useSystemColorMode: false,
   initialColorMode: "dark",
 };
-
+const data = [
+  { year: '2011', earnings: 13000 },
+  { year: '2012', earnings: 16500 },
+  { year: '2013', earnings: 14250 },
+  { year: '2014', earnings: 19000 }
+ ];
 // extend the theme
 export const theme = extendTheme({ config });
 
 export default function App() {
+  const [chartData, setData] = useState(data);
+  const addData = () => {
+    var maxYear = 2015;
+    var d = [...chartData]
+    var obj = {year: `${maxYear}`, earnings: Math.random() * (20000 - 10000) + 10000}
+    d.push(obj)
+    setData(d)
+    console.log(chartData);
+    maxYear++
+   }
+
+   useEffect(()=>{
+     setInterval(()=> {
+       setData([]);
+       addData();
+     },5500);
+   },[chartData])
   return (
     <NativeBaseProvider>
       <Center
@@ -33,7 +59,16 @@ export default function App() {
         flex={1}
       >
         <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
+        <VictoryChart width={350} theme={VictoryTheme.material}>
+          {chartData && 
+
+          <VictoryLine data={chartData} animate={{
+            duration: 500,
+            easing: 'sinIn'
+          }} y="earnings" />}
+        </VictoryChart>
+        <Button onPress={addData}>Add Earnings</Button>
+          {/* <NativeBaseIcon />
           <Heading size="lg">Welcome to NativeBase</Heading>
           <HStack space={2} alignItems="center">
             <Text>Edit</Text>
@@ -45,7 +80,7 @@ export default function App() {
               Learn NativeBase
             </Text>
           </Link>
-          <ToggleDarkMode />
+          <ToggleDarkMode /> */}
         </VStack>
       </Center>
     </NativeBaseProvider>
