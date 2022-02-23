dc04fbe3b31abd84573d2aeda3d58eb904db66e8
OmarMohamed156 <om658036@gmail.com>
2022-02-23 23:24:23 +0200

    task-0 mobile app finished

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React ,{useEffect, useState} from "react";
 import {Text,HStack,Center,NativeBaseProvider,extendTheme,VStack,Button,ScrollView} from "native-base";
 import { VictoryChart,VictoryTheme,VictoryLine, VictoryLabel, VictoryAxis} from "victory-native";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faTemperatureThreeQuarters,faDroplet, faEraser } from "@fortawesome/free-solid-svg-icons";
+import { faTemperatureThreeQuarters,faDroplet, faEraser, faBell } from "@fortawesome/free-solid-svg-icons";
 import { Dimensions } from "react-native";
 import axios from "axios";
 // Define the config
@@ -24,7 +24,7 @@ export default function App() {
   const [humData, setHumData] = useState([]);
   // const [alarmStatus,setAlarmStatus]= useState();
   const [alarmStatusMessage,setAlarmStatusMessage] = useState('Set');
-
+  //plot temp data
   const PlotTemperatureData=()=>{
     setInterval(()=>{
       axios.get('http://remote-monitoring-api.herokuapp.com/readings/temp',{
@@ -32,18 +32,25 @@ export default function App() {
           'Content-Type': 'application/json'
       }
       }).then((res)=>{
-        console.log(res.data.temperatures);
         setTempData(res.data.temperatures);
+      }).catch((err)=>{
+        throw(err.message)
       })
     },12000)
   }
+  //plot humitiy data
   const PlotHumidityData=()=>{
-
-    // setHumData(data);
-  }
-  const deleteData=()=>{
-    setTempData([]);
-    setHumData([]);
+    setInterval(()=>{
+      axios.get('https://remote-monitoring-api.herokuapp.com/readings/humidity',{
+        headers: {
+          'Content-Type': 'application/json'
+      }
+      }).then((res)=>{
+        setHumData(res.data.humidities);
+      }).catch((err)=>{
+        throw(err.message)
+      })
+    },12000)
   }
   const setAlarm = (status)=>{
     axios.post('https://remote-monitoring-api.herokuapp.com/control/alarm',{
@@ -59,7 +66,6 @@ export default function App() {
   const toggleAlarm=()=>{
     axios.get('https://remote-monitoring-api.herokuapp.com/control/alarm')
     .then((res)=>{
-      console.log(res.data.alarm);
       if(res.data.alarm == 1){
         setAlarmStatusMessage('Set')
         setAlarm(0);
@@ -70,34 +76,39 @@ export default function App() {
       }
     })
     .catch((err)=>{
-      console.log(err.message);
+      throw(err.message)
     })
   }
-  //  useEffect(()=>{
-  //    setInterval(()=> {
-  //      setData([]);
-  //      addData();
-  //    },5500);
-  //  },[chartData])
+
+  const deleteData=()=>{
+    setTempData([]);
+    setHumData([]);
+  }
+
   return (
     <NativeBaseProvider>
-      <Center height='89%' >
+      <Center height='94%' >
         <ScrollView>
         <VStack safeArea > 
             <Text mt={1} textAlign='center' color='tertiary.600'> <FontAwesomeIcon  color="#059669" icon={faTemperatureThreeQuarters}/> Tempreature Graph</Text>
             <Center>
               <HStack >
                 <ScrollView mx={5} horizontal={true}>
-                    <VictoryChart domain={{x:[0,100]}} width={Dimensions.get('window').width} theme={VictoryTheme.material} >
-                      <VictoryLine style={{
+                    <VictoryChart  width={Dimensions.get('window').width} theme={VictoryTheme.material} >
+                      <VictoryLine  style={{
                         data: { stroke: '#059669'}
                       }} animate={{
                         duration:5000,
                         easing:'sinIn'
                       }}
                       data={tempData}  y="temperature" />
-                      <VictoryAxis crossAxis label='Time' axisLabelComponent={<VictoryLabel dy={25}  textAnchor='inherit' />}/>
-                      <VictoryAxis dependentAxis crossAxis  label='Temperature'   axisLabelComponent={<VictoryLabel  dy={-30} textAnchor='inherit' />}/>
+                      <VictoryAxis style={{axisLabel:{
+                        fontSize: 15, fill:'#059669'
+                      }}} crossAxis label='Time' axisLabelComponent={<VictoryLabel dy={25}  textAnchor='inherit' />}/>
+                      <VictoryAxis style={{axisLabel:{
+                        fontSize: 15, fill:'#059669'
+                      }}} dependentAxis crossAxis  label='Temperature'   axisLabelComponent={<VictoryLabel  dy={-30} textAnchor='inherit' />}/>
+                      
                     </VictoryChart>
                 </ScrollView>
               </HStack>
@@ -106,16 +117,21 @@ export default function App() {
             <Center>
               <HStack >
                 <ScrollView mx={5} horizontal={true}>
-                    <VictoryChart domain={{x:[0,100]}} width={Dimensions.get('window').width-25} theme={VictoryTheme.material}>
+                    <VictoryChart  width={Dimensions.get('window').width-25} theme={VictoryTheme.material}>
                       <VictoryLine style={{
-                        data: { stroke: '#059669'}
+                        data: { stroke: '#06b6d4'}
                       }}
+                      y='humidity'
                       animate={{
                         duration:5000,
                         easing: 'sinOut'
-                      }} data={humData} x="year" y="earnings" />
-                      <VictoryAxis crossAxis label='Time' axisLabelComponent={<VictoryLabel dy={25}  textAnchor='inherit' />}/>
-                      <VictoryAxis dependentAxis crossAxis  label='Humidity'   axisLabelComponent={<VictoryLabel  dy={-30} textAnchor='inherit' />}/>
+                      }} data={humData}  />
+                      <VictoryAxis style={{axisLabel:{
+                        fontSize: 15, fill:'#06b6d4'
+                      }}} crossAxis label='Time' axisLabelComponent={<VictoryLabel dy={25}  textAnchor='inherit' />}/>
+                      <VictoryAxis style={{axisLabel:{
+                        fontSize: 15, fill:'#06b6d4'
+                      }}} dependentAxis crossAxis  label='Humidity'   axisLabelComponent={<VictoryLabel  dy={-30} textAnchor='inherit' />}/>
                     </VictoryChart>
                 </ScrollView>
               </HStack>
@@ -123,17 +139,21 @@ export default function App() {
           </VStack>
         </ScrollView>
       </Center>
-      <Center height='11%'>
+      <Center height='6%'>
         <VStack>
-          <HStack justifyContent='space-around' >
-            <Button borderRadius={50} mx={3}  colorScheme="tertiary"  onPress={PlotTemperatureData}><Text color='white' >Get Temperature <FontAwesomeIcon  color="#ffffff" icon={faTemperatureThreeQuarters}/></Text></Button>
-            <Button  borderRadius={50} mx={3}   colorScheme="primary" onPress={PlotHumidityData}><Text color='white'>Get Humidity <FontAwesomeIcon  color="#ffffff" icon={faDroplet}/></Text></Button>
-          </HStack>
-          <Center>
-            <HStack my={1}>
-              <Button  borderRadius={50} colorScheme='danger' onPress={toggleAlarm} on ><Text color='white'>{alarmStatusMessage} Alarm <FontAwesomeIcon  color="#ffffff" icon={faEraser}/></Text></Button>
+          <ScrollView horizontal={true}>
+            <HStack p={1}  my={1} justifyContent='space-around' >
+              <Button borderRadius={50} mx={3}  colorScheme="tertiary"  onPress={PlotTemperatureData}><Text color='white' >Get Temperature <FontAwesomeIcon  color="#ffffff" icon={faTemperatureThreeQuarters}/></Text></Button>
+              <Button  borderRadius={50} mx={3}   colorScheme="primary" onPress={PlotHumidityData}><Text color='white'>Get Humidity <FontAwesomeIcon  color="#ffffff" icon={faDroplet}/></Text></Button>
+              <Button  borderRadius={50} colorScheme='yellow' onPress={toggleAlarm} on ><Text color='white'>{alarmStatusMessage} Alarm <FontAwesomeIcon  color="#ffffff" icon={faBell}/></Text></Button>
+              <Button  borderRadius={50} mx={3}   colorScheme="danger" onPress={deleteData}><Text color='white'> Clear Graph <FontAwesomeIcon  color="#ffffff" icon={faEraser}/></Text></Button>
             </HStack>
-          </Center>
+          </ScrollView>
+          {/* <Center>
+          <HStack my={1} justifyContent='space-around' >
+
+          </HStack>
+          </Center> */}
         </VStack>
       </Center>
     </NativeBaseProvider>
